0253043fb99c9b59cdb779e1afa9e5559de9bf7c
Tim Clark <oeomusic@gmail.com>
2017-02-27 14:22:08 -0800

    connects tone.js and tests click handler

--- ambient_app/src/App.js
+++ ambient_app/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Chords from './chords/Chords';
+import Tone from 'tone';
 
 class App extends Component {
+
+  componentDidMount() {
+    Tone.Transport.start();
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Chords />
       </div>
     );
   }
---
+++ ambient_app/src/chords/Chords.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Tone from 'tone';
+import './Chords.css';
+import StartButton from './StartButton';
+import StopButton from './StopButton';
+
+class Chords extends Component {
+
+  constructor(){
+    super();
+    this.startPattern = this.startPattern.bind(this);
+  }
+
+  startPattern() {
+    var synth = new Tone.PolySynth(8, Tone.FMSynth).toMaster();
+    var chord = new Tone.Event(function(rate){
+      synth.triggerAttackRelease(["D4", "F4", "A4", "B4"], "4n");
+    }, "4n");
+    // Tone.Transport.start();
+    chord.start();
+    console.log('fuuuck')
+  }
+
+  render() {
+
+    return (
+      <div className="chords">
+        <h1>CHORDS</h1>
+        <StartButton onClick={this.startPattern} />
+        <StopButton />
+      </div>
+    );
+  }
+}
+
+export default Chords;
