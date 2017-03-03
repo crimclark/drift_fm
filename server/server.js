cbf9046379558cb0bff231a227c74355c41546d6
Tim Clark <oeomusic@gmail.com>
2017-03-03 10:39:49 -0800

    enables cors

--- server/server.js
+++ server/server.js
@@ -7,11 +7,7 @@ const app = express();
 
 require('./db/config');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(require('cors')());
 
 app.use(morgan('dev'));
 // app.use(express.static(path.join(__dirname, 'public')))
