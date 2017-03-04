b592b80e47eff5daff4898a7f51e4da88dc2fb5a
Tim Clark <oeomusic@gmail.com>
2017-03-04 15:09:20 -0800

    refactors allbutton to transportbutton and adds transport control

---
+++ ambient_app/src/components/buttons/TransportButton.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class TransportButton extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { mode, pattern, handleClick } = this.props;
+    if (mode === 'all') {
+      const [melodyPattern, chordPattern, sampleInstrument] = pattern;
+      handleClick(melodyPattern, chordPattern, sampleInstrument);
+    }
+  }
+
+  render() {
+    return (
+        <button className="pure-button" onClick={this.handleClick}>{this.props.children}</button>
+      )
+  }
+}
+
+export default TransportButton;
--- ambient_app/src/components/global/Global.js
+++ ambient_app/src/components/global/Global.js
@@ -1,42 +1,28 @@
-import React, { Component } from 'react';
-import AllButton from '../buttons/AllButton';
+import React from 'react';
 import Transpose from '../controls/Transpose';
+import Transport from '../controls/Transport';
 import BPMSlider from '../buttons/BPMSlider';
 import './global.css';
 import { melodyPattern } from '../melody/melodyInstrument';
 import { chordPattern } from '../chords/chordInstrument';
 import sampleInstrument from '../sample/SampleInstrument';
 
-class Global extends Component {
-
-  render() {
-    return (
-      <div className="instrument global">
-        <h1>G L O B A L</h1>
-        <div>
-          Transpose:
-          <Transpose detuneHandler={this.props.detuneHandler} synth='all' plus={100} minus={-100} />
-        </div>
-        <div>
-          <BPMSlider min={5} max={105} value={55} />
-        </div>
-        <div>
-          <AllButton handleStart={this.props.startAll}
-            melodyPattern={melodyPattern}
-            chordPattern={chordPattern}
-            samplePattern={sampleInstrument}>
-            START ALL
-          </AllButton>
-          <AllButton handleStart={this.props.stopAll}
-            melodyPattern={melodyPattern}
-            chordPattern={chordPattern}
-            samplePattern={sampleInstrument}>
-            STOP ALL
-          </AllButton>
-        </div>
+const Global = ({detuneHandler, startAll, stopAll}) => {
+  return (
+    <div className="instrument global">
+      <h1>G L O B A L</h1>
+      <div>
+        Transpose:
+        <Transpose detuneHandler={detuneHandler} synth='all' plus={100} minus={-100} />
       </div>
-      )
-  }
+      <div>
+        <BPMSlider min={5} max={105} value={55} />
+      </div>
+      <Transport handleStart={startAll} handleStop={stopAll}
+      pattern={[melodyPattern, chordPattern, sampleInstrument]}
+      startText='START ALL' stopText='STOP ALL' mode='all' />
+    </div>
+    )
 }
 
 export default Global;
