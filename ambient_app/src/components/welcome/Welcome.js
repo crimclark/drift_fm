8236ce61d42b8a2be9964f049df6d107f323e509
Tim Clark <oeomusic@gmail.com>
2017-03-15 10:26:08 -0700

    styles welcome page and fixes typo

--- ambient_app/src/components/welcome/Welcome.js
+++ ambient_app/src/components/welcome/Welcome.js
@@ -7,17 +7,18 @@ const Welcome = () => {
     <div>
       <div id="welcome-container">
         <h1 id="welcome">WELCOME</h1>
-        <div id="welcome-text">
-          Drift.FM is a web app that generates random music in your browser.
-          To begin, click on any of the instruments in the above navigation bar
-          and press the "Start" button.  Play around with the instrument settings
-          to hear how they alter the sound, or simply let the sound drift along
-          in the background as you go about the rest of your day.
-
+        <div id="text-container">
+          <div id="welcome-text">
+            Drift.FM is a web app that generates random ambient music in your browser.
+            To begin, click on any of the instruments in the above navigation bar
+            and press the "Start" button.  Play around with the settings
+            to hear how they alter the sound, or simply let the music drift along
+            in the background as you go about your day.
+          </div>
         </div>
       </div>
       <p id="disclaimer">Note: The Web Audio API is an experimental technology which is not yet
-      supported by all browsers.  For the best exprience, please consider using
+      supported by all browsers.  For the best experience, please use
       the latest versions of Google Chrome or Mozilla Firefox.</p>
     </div>
     )
