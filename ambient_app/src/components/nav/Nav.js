5c2aa03b81b4326556e7e905089141e7a6f2da63
Tim Clark <oeomusic@gmail.com>
2017-05-10 22:37:16 -0700

    refactors nav to stateless component

--- ambient_app/src/components/nav/Nav.js
+++ ambient_app/src/components/nav/Nav.js
@@ -1,35 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NavLink from './NavLink';
 import './nav.css';
 
-class Nav extends Component {
-
-  // destructure handleClick
-  render() {
-    return (
-      <div className="pure-menu pure-menu-horizontal">
-        <ul className="pure-menu-list">
-          <NavLink link={"CHORDS"} handleClick={this.props.handleClick} />
-          <NavLink link={"MELODY"} handleClick={this.props.handleClick}/>
-          <NavLink link={"SAMPLE"} handleClick={this.props.handleClick} />
-          <NavLink link={"GLOBAL"} handleClick={this.props.handleClick} />
-        </ul>
-      </div>
-    )
-  }
+const Nav = ({ handleClick }) => {
+  return (
+    <div className="pure-menu pure-menu-horizontal">
+      <ul className="pure-menu-list">
+        <NavLink link={"CHORDS"} handleClick={handleClick} />
+        <NavLink link={"MELODY"} handleClick={handleClick}/>
+        <NavLink link={"SAMPLE"} handleClick={handleClick} />
+        <NavLink link={"GLOBAL"} handleClick={handleClick} />
+      </ul>
+    </div>
+  )
 }
-
-// const Nav = ({ handleClick }) => {
-//   return (
-//     <div className="pure-menu pure-menu-horizontal">
-//       <ul className="pure-menu-list">
-//         <NavLink link={"CHORDS"} handleClick={handleClick} />
-//         <NavLink link={"MELODY"} handleClick={handleClick}/>
-//         <NavLink link={"SAMPLE"} handleClick={handleClick} />
-//         <NavLink link={"GLOBAL"} handleClick={handleClick} />
-//       </ul>
-//     </div>
-//   )
-// }
 
 export default Nav;
